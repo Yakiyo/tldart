57e8b42e9964b4439a8e2a7a5060f7b01c8ce09d
Yakiyo <yakiyosama5855@gmail.com>
2023-04-03 06:51:17 +0000

    feat: basic flags handling

--- bin/tldr.dart
+++ bin/tldr.dart
@@ -1,17 +1,33 @@
 import 'package:fast_tldr/fast_tldr.dart';
 import 'package:args/args.dart' show ArgResults;
-import 'package:ansi/ansi.dart' show ansi;
+import 'package:ansi/ansi.dart' show Ansi;
 import 'dart:io' show exit;
 
 void main(List<String> arguments) {
   late final ArgResults args;
+  final ansi = Ansi();
 
   try {
     args = parser.parse(arguments);
   } on FormatException catch (e) {
-    eprint("${ansi.red("FORMATERR:")} $e");
+    eprint("${ansi.red("ARGERR:")} ${e.message}");
     exit(1);
   }
 
-  print(args);
+  if (args['version'] == true) {
+    print("tldr cli version: $version");
+    exit(0);
+  }
+
+  if (args['update'] == true) {
+    // TODO: Help
+    print("Help message");
+    exit(0);
+  }
+
+  if (arguments.isEmpty || args.rest.isEmpty) {
+    eprint("${ansi.red("ARGERR:")} Missing arguments. Use the `${ansi.bold("--help")}` flag to see usage.");
+    exit(1);
+  }
+
 }
