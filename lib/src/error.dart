52266820181d2d0fd6bad8aaf23d2f6f54393f67
Yakiyo <yakiyosama5855@gmail.com>
2023-04-03 16:12:25 +0000

    feat: error handling

---
+++ lib/src/error.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: constant_identifier_names
+
+/// Set of errors to be used for handling specific events
+enum Errors {
+  MissingIndex,
+  InvalidIndex,
+  MissingCache,
+  MissingCommandFile,
+  InvalidCommandLang,
+  InvalidCommandPlatform,
+  InvalidDefaultPlatform,
+}
+
+/// Exceptions thrown by the library
+class LibException implements Exception {
+  final Errors code;
+  final String? message;
+  const LibException(this.code, {this.message});
+}
