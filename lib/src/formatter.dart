d569688bf61fa978fa4e5918257077c1914aef05
Yakiyo <yakiyosama5855@gmail.com>
2023-04-03 16:54:54 +0000

    feat: formatter

---
+++ lib/src/formatter.dart
@@ -0,0 +1,28 @@
+import 'package:ansi/ansi.dart' show Ansi;
+
+/// Format a markdown file
+List<String> render(String content) {
+  final ansi = Ansi();
+  final res = <String>[];
+  res[0] = '';
+  for (var line in content.split('\n')) {
+    if (line.startsWith('#')) {
+      continue; // skip command name
+    } else if (line.startsWith('>')) {
+      // Command description
+      res[0] += '\n${line.substring(1)}';
+    } else if (line.startsWith('-')) {
+      // Example description
+      res.add(ansi.blue(line.substring(1)));
+    } else if (line.startsWith('`')) {
+      // Example
+      line = line.substring(
+          1, line.length - 1); // Remove ` from the first and last position
+      // TODO: Replace curly braces with underline
+      line = line.replaceAll(
+          RegExp(r'{{|}}'), ''); // replace curly braces in examples
+      res.add('      ${ansi.cyan(line)}');
+    }
+  }
+  return res;
+}
--- lib/tldart.dart
+++ lib/tldart.dart
@@ -1,5 +1,6 @@
 export 'src/cli.dart';
 export 'src/cache.dart';
+export 'src/formatter.dart';
 export 'src/util.dart' show debug, eprint, showHelp;
 
 /// Package version
