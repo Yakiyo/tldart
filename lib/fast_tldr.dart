bb2ec8cf545dcf996bd3fb9e66faf132710b0bc5
Yakiyo <yakiyosama5855@gmail.com>
2023-04-03 06:45:54 +0000

    feat: more funcs and vars

--- lib/fast_tldr.dart
+++ lib/fast_tldr.dart
@@ -1,3 +1,5 @@
-
 export 'src/cli.dart';
 export 'src/util.dart';
+
+/// Package version
+const version = "1.0.0";
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,7 +1,9 @@
 import 'dart:io' show stderr;
 
+/// Print debug logs. This should only print them if DEBUG env is set to 1
 void debug(dynamic message) {}
 
+/// Print errors to std err
 void eprint(dynamic error) {
   stderr.writeln(error);
 }
