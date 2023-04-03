52cc2802de136b76e48423e759eb14f5e28c9131
Yakiyo <yakiyosama5855@gmail.com>
2023-04-03 06:53:26 +0000

    feat: debug function

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,9 +1,15 @@
-import 'dart:io' show stderr;
+import 'dart:io' show stderr, Platform;
 
 /// Print debug logs. This should only print them if DEBUG env is set to 1
-void debug(dynamic message) {}
+void debug(dynamic message) {
+  final env = Platform.environment['DEBUG'];
+  if (env == null || env.isEmpty) return;
+  print(message);
+  return;
+}
 
 /// Print errors to std err
 void eprint(dynamic error) {
   stderr.writeln(error);
+  return;
 }
